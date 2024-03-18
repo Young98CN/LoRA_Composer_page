ad90d1a8d5cd930fcaedb51fb9dc0213be59e5d8
Young98CN <834273418@qq.com>
2024-03-18 10:08:07 +0800

    first commit

---
+++ sm/assets/teaser-data.js
@@ -0,0 +1,20 @@
+file_paths = {
+    "5real": ["sm/assets/lora_composer_res/five_concepts/15958.png","sm/assets/lora_composer_res/five_concepts/5real_layout.png"],
+    "5real2": ["sm/assets/lora_composer_res/five_concepts/5real2_10966.png", "sm/assets/lora_composer_res/five_concepts/5real_layout.png"],
+    "5anime": ["sm/assets/lora_composer_res/five_concepts/5anime2_3251.png", "sm/assets/lora_composer_res/five_concepts/5anime_layout.png"],
+    "5anime2": ["sm/assets/lora_composer_res/five_concepts/5anime3_11441.png", "sm/assets/lora_composer_res/five_concepts/5anime_layout.png"],
+   
+    "4real": ["sm/assets/lora_composer_res/four_concepts/4real2_12999.png","sm/assets/lora_composer_res/four_concepts/4real_layout.png"],
+    "4real2": ["sm/assets/lora_composer_res/four_concepts/4real3_5278.png", "sm/assets/lora_composer_res/four_concepts/4real_layout.png"],
+    "4anime": ["sm/assets/lora_composer_res/four_concepts/4anime2_16849.png", "sm/assets/lora_composer_res/four_concepts/4anime_layout.png"],
+
+    "3real": ["sm/assets/lora_composer_res/three_concepts/paper_14297.png","sm/assets/lora_composer_res/three_concepts/2dog+1cat_layout.png"],
+    "shake": ["sm/assets/lora_composer_res/three_concepts/1625.png","sm/assets/lora_composer_res/three_concepts/shake_layout.png"],
+    "3real2": ["sm/assets/lora_composer_res/three_concepts/3real_5278.png", "sm/assets/lora_composer_res/three_concepts/3real_layout.png"],
+    "3anime2": ["sm/assets/lora_composer_res/three_concepts/3anime_1977.png", "sm/assets/lora_composer_res/three_concepts/3anime_layout.png"],
+
+    "2real": ["sm/assets/lora_composer_res/two_concepts/lora_composer_9023.png","sm/assets/lora_composer_res/two_concepts/2real_layout.png"],
+    "2real2": ["sm/assets/lora_composer_res/two_concepts/2real2_9023.png", "sm/assets/lora_composer_res/two_concepts/2real_layout.png"],
+    "2anime": ["sm/assets/lora_composer_res/two_concepts/2boys_6738.png", "sm/assets/lora_composer_res/two_concepts/2boys_layout.png"],
+    "2anime2": ["sm/assets/lora_composer_res/two_concepts/2girls_12494.png", "sm/assets/lora_composer_res/two_concepts/2girls_layout.png"],
+}
